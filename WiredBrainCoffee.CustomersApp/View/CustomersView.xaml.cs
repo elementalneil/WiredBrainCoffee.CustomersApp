1959748d0cdee4ada4fbdd4fc1acc07f54a7eb4c
elementalneil <nayek.sankhanil@gmail.com>
2024-04-17 13:45:52 +0530

    Added enum to denote the position of users list

--- WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -21,15 +21,7 @@ namespace WiredBrainCoffee.CustomersApp.View {
         }
 
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e) {
-            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
-
-            //var newColumn = column == 0 ? 2 : 0;
-            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
-
-            var column = Grid.GetColumn(customerListGrid);
-
-            var newColumn = column == 0 ? 2 : 0;
-            Grid.SetColumn(customerListGrid, newColumn);
+            _viewModel.MoveNavigation();
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e) {
--- WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using WiredBrainCoffee.CustomersApp.Data;
@@ -8,20 +10,29 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel {
 
     public class CustomersViewModel : ViewModelBase {
         private readonly ICustomerDataProvider _customerDataProvider;
-        private Customer? _selectedCustomer;
+        private CustomerItemViewModel? _selectedCustomer;
+        private NavigationSide _navigationColumn;
 
         public CustomersViewModel(ICustomerDataProvider customerDataProvider) {
             _customerDataProvider = customerDataProvider;
         }
 
-        public ObservableCollection<Customer> Customers { get; } = new();
+        public ObservableCollection<CustomerItemViewModel> Customers { get; } = new();
 
-        public Customer? SelectedCustomer { 
+        public CustomerItemViewModel? SelectedCustomer { 
             get => _selectedCustomer;
             set {
                 _selectedCustomer = value;
                 RaisePropertyChanged();
             } 
+        }
+
+        public NavigationSide NavigationColumn { 
+            get => _navigationColumn; 
+            private set { 
+                _navigationColumn= value; 
+                RaisePropertyChanged();
+            }
         }
 
         public async Task LoadAsync() {
@@ -32,15 +43,27 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel {
             var customers = await _customerDataProvider.GetAllAsync();
             if (customers != null) {
                 foreach (var customer in customers) {
-                    Customers.Add(customer);
+                    Customers.Add(new CustomerItemViewModel(customer));
                 } 
             }
         }
 
         internal void Add() {
             var newCustomer = new Customer { FirstName = "New" };
-            Customers.Add(newCustomer);
-            SelectedCustomer = newCustomer;
+            var newCustomerViewModel = new CustomerItemViewModel(newCustomer);
+            Customers.Add(newCustomerViewModel);
+            SelectedCustomer = newCustomerViewModel;
         }
+
+        internal void MoveNavigation() {
+            NavigationColumn = NavigationColumn == NavigationSide.Left 
+                ? NavigationSide.Right 
+                : NavigationSide.Left;
+        }
+    }
+
+    public enum NavigationSide {
+        Left,
+        Right
     }
 }
